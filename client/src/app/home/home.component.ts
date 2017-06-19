c1a8ea0cbea41872ebfaa8c729ddf4362b0166fd
seifElislam <seif.eleslam1990@gmail.com>
2017-06-19 17:58:00 +0200

    add list o of users to home and selected friend

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -14,11 +14,14 @@ export class HomeComponent implements OnInit {
   user={id:0,username:"",status:0}
   users:any[]=[]
   currentFriend:any
+  currentUser:any
   constructor( private notify:NotifyService, private handler:ChatHandlerService)
   {
       this.notify.startChannel()
       this.notify.sendMessage('hello world')
       this.notify.reciveMessage()
+      this.currentUser =  JSON.parse(localStorage.getItem('currentUser'));
+      console.log("me :", this.currentUser.username)
 
    }
 
@@ -27,18 +30,23 @@ export class HomeComponent implements OnInit {
     this.handler.get_users().subscribe(data=>{
       this.list = data
       for (var i = 0; i < this.list.length; i++){
-
         console.log("i :",this.list[i].fields['username'])
-
-         this.user.id = this.list[i].pk;
-         this.user.username = this.list[i].fields['username'];
-
-        this.users.push(this.user)
+        let user={id:0,username:"",status:"offline"}
+        if(this.currentUser.id != this.list[i].pk){
+         user.id = this.list[i].pk;
+         user.username = this.list[i].fields['username'];
+         this.users.push(user)
+       }
       }
        console.log('my list:',this.users)
     },error=>{
       console.log('error:',error)
     })
+  }
+
+  onSelect(user: User): void {
+    console.log("selection:",user)
+    this.currentFriend = user
   }
 
 }
